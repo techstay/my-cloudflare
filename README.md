0f796be0b6821ce0562784730f33dbe177a1f0b3
techstay <lovery521@gmail.com>
2024-02-24 03:31:17 +0800

    ✨ glados签到

---
+++ README.md
@@ -0,0 +1,8 @@
+# My-Cf-Workers
+
+```sh
+wrangler secret put TG_BOT_TOKEN
+wrangler secret put TG_CHAT_ID
+wrangler secret put GLADOS_K
+wrangler secret put GLADOS_K_SIG
+```
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,73 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	TG_BOT_TOKEN: string,
+	TG_CHAT_ID: string,
+	GLADOS_K: string,
+	GLADOS_K_SIG: string
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async scheduled (event, env, ctx) {
+		console.log('cron processed')
 	},
-};
+	async fetch (request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		async function fetchHitokoto () {
+			const response = await fetch('https://international.v1.hitokoto.cn')
+			const { hitokoto: hitokotoText, from: hitokotoFrom } = await response.json() as { hitokoto: string, from: string }
+			return `${hitokotoText}——${hitokotoFrom}`
+		}
+
+		async function pushTelegramBot (content: string) {
+			const pushUrl = `https://api.telegram.org/bot${env.TG_BOT_TOKEN}/sendMessage`
+			const form = new FormData()
+			form.append('chat_id', env.TG_CHAT_ID)
+			form.append('text', `${content}\n`)
+			await fetch(pushUrl, {
+				method: 'POST',
+				body: form
+			})
+		}
+
+		async function checkinGlados () {
+			const checkinUrl = 'https://glados.rocks/api/user/checkin'
+			const checkinStatusUrl = 'https://glados.rocks/api/user/status'
+			const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
+			const payload = { 'token': 'glados.one' }
+			const cookies: Record<string, string> = {
+				'koa:sess': env.GLADOS_K,
+				'koa:sess.sig': env.GLADOS_K_SIG
+			}
+			const headers = {
+				'Origin': 'https://glados.rocks',
+				'User-Agent': userAgent,
+				'Content-Type': 'application/json;charset=UTF-8',
+				Cookie: Object.keys(cookies).map(key => `${key}=${cookies[key]}`).join(';')
+			}
+
+			const checkinResponse = await fetch(checkinUrl, {
+				headers: headers,
+				method: 'POST',
+				body: JSON.stringify(payload),
+			})
+			const checkinResponseJson = await checkinResponse.json()
+			// console.log(checkinResponseJson)
+
+			const { code: checkinResultCode, message: checkinMessage } = checkinResponseJson as { code: string, message: string }
+
+			if (checkinResultCode !== '1') {
+				await pushTelegramBot(`GLADOS签到异常： ${checkinMessage}`)
+			} else {
+				await pushTelegramBot(`GLADOS签到成功： ${checkinMessage}`)
+
+				const checkinStatusResponse = await fetch(checkinStatusUrl, {
+					headers: headers
+				})
+				const { data: { email }, data: { leftDays } } = await checkinStatusResponse.json() as { data: { email: string, leftDays: string } }
+				await pushTelegramBot(`账号${email}，剩余时长${Math.floor(parseFloat(leftDays))}天`)
+				await pushTelegramBot(await fetchHitokoto())
+			}
+		}
+
+		await checkinGlados()
+		return new Response(await fetchHitokoto())
+	}
+} satisfies ExportedHandler
