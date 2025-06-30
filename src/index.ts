0dca2e90b72302ae6acbc4569147b00de11323b3
techstay <lovery521@gmail.com>
2025-06-30 16:11:52 +0800

    update dependencies

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,11 @@ import { checkinGlados } from './glados';
 import { hitokoto } from './toolkit';
 
 export default {
-	async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
+	async scheduled (event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
 		await Promise.all([checkinGlados(env)]);
 	},
 
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+	async fetch (request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		return new Response(await hitokoto());
 	},
 };
